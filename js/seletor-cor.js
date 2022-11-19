d68d3bf4a10927063f6bc2d0535a3ced8a6f03e2
Sayonara <sayonara.crestani@gmail.com>
2022-11-19 11:50:31 -0300

    adds the html files for specific itens

---
+++ js/seletor-cor.js
@@ -0,0 +1,13 @@
+const botao = document.querySelector("#visualize");
+const body = document.querySelector('body');
+
+botao.addEventListener('click', function() {
+  const box = document.getElementById("color");
+  const selectedColor = box.value;
+
+  body.style.backgroundColor = selectedColor;
+
+  if(body.style.backgroundColor !== selectedColor) {
+    alert("Please, use the CSS colors!")
+  }
+});
